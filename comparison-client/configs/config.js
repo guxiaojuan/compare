5cdd6555743a55ce84488cc51ba3a61bedda92f8
guxiaojuan <1634747911@qq.com>
2017-04-05 09:55:55 +0800

    pull 代码

--- comparison-client/configs/config.js
+++ comparison-client/configs/config.js
@@ -1,7 +1,8 @@
 var hostname='http://localhost'
 //hostname="192.168.61.1"
 var port='8080'
-var server=hostname+':'+port+'/'
+//var server=hostname+':'+port+'/'
+var server='https://lichun.nat123.com'
 module.exports={
     'server':server
 }
--- comparison-client/pages/collect/collect.js
+++ comparison-client/pages/collect/collect.js
@@ -19,6 +19,21 @@ Page({
         title:'请先登录'
       });
     }
-
+   wx.request({
+     url: 'https://lichun.net123.com',
+     data: {},
+     method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+     // header: {}, // 设置请求的 header
+     success: function(res){
+       console.log('success')
+     },
+     fail: function() {
+       // fail
+       console.log(fail);
+     },
+     complete: function() {
+       // complete
+     }
+   })
   }
 })
